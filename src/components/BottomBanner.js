c3d17d05b6d198f3e9ee19b6588d91ceec672b48
Danny <DannyMo511@gmail.com>
2019-09-27 10:59:16 +0300

    created layout, added top and bottom banners, styling for the scrollbar and backgrounds

---
+++ src/components/BottomBanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BottomBanner = () =>{
+	return (
+			<div className="ma1 pa1">
+				<h3> developed by 
+				<a className="no-underline" 
+				   href="https://github.com/DannyMo511"
+				   target="_blank"
+				   rel="noopener noreferrer"> DannyMo </a>
+				 as a react mockup project </h3>
+			</div>
+			)
+}
+
+export default BottomBanner;
---
+++ src/components/TopBanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './cat-icon.png';
+
+const TopBanner = () =>{
+	return (
+			<div className="pa3">
+				<img className="v-mid" src={logo} alt="icon" width="43px" height="49px"/>
+				<h1 className="di v-mid"> Cat Gag </h1>
+			</div>
+			)
+}
+
+export default TopBanner;
